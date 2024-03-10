562a11739c9d45ce18e1a5e5d74f39fb7a5e8ba9
Robert Hull <robertjhull@protonmail.com>
2024-03-09 21:42:10 -0800

    Minor style fixes

--- components/preview/preview-post.vue
+++ components/preview/preview-post.vue
@@ -99,8 +99,88 @@ const datePublished = (datetime: string): string => formatLongDate(datetime);
     }
   }
 
-  .post-content p {
-    margin: 2rem 0;
+  .post-content {
+    ul,
+    ol {
+      padding: 0 1rem;
+    }
+
+    h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    h6 {
+      line-height: 1.1;
+    }
+
+    code {
+      background-color: rgba(#616161, 0.1) !important;
+      color: #616161;
+    }
+
+    pre {
+      background: #0d0d0d;
+      border-radius: 0.5rem;
+      color: #fff;
+      font-family: "JetBrainsMono", monospace;
+      padding: 0.75rem 1rem;
+
+      code {
+        background: none;
+        color: inherit;
+        font-size: 0.8rem;
+        padding: 0;
+      }
+    }
+
+    mark {
+      background-color: #faf594;
+    }
+
+    img {
+      height: auto;
+      max-width: 100%;
+    }
+
+    hr {
+      margin: 1rem 0;
+    }
+
+    blockquote {
+      border-left: 2px solid rgba(#0d0d0d, 0.1);
+      padding-left: 1rem;
+    }
+
+    hr {
+      border: none;
+      border-top: 2px solid rgba(#0d0d0d, 0.1);
+      margin: 2rem 0;
+    }
+
+    ul[data-type="taskList"] {
+      list-style: none;
+      padding: 0;
+
+      li {
+        align-items: center;
+        display: flex;
+
+        > label {
+          flex: 0 0 auto;
+          margin-right: 0.5rem;
+          user-select: none;
+        }
+
+        > div {
+          flex: 1 1 auto;
+        }
+      }
+    }
+
+    p {
+      margin: 2rem 0;
+    }
   }
 }
 </style>
